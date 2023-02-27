3e1bc518cc0e94107dcc84f74b07d50023c94c3a
SerxanAli <aliyev.s.v1990@gmail.com>
2023-02-27 12:52:24 +0400

    2.0

--- Rock-Paper-Scissors/ViewController2.swift
+++ Rock-Paper-Scissors/ViewController2.swift
@@ -14,6 +14,7 @@ class ViewController2: UIViewController {
     
     var userScore = 0 {
         didSet{ UserScore.text = "\(userScore)" }
+        
     }
     
     var compScore = 0 {
@@ -32,56 +33,53 @@ class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        
-        
+      
     }
+    
+   
     
     @IBAction func selectedHandAction(_ sender: UIButton) {
        
-        
-      
-            
             if let compChosenHand = randomHand.randomElement() {
                 if let chosinHand = sender.titleLabel?.text {
-                    if (compChosenHand == "✋🏻") && (chosinHand == "👊🏻") {
-                        compScore += 1
-                    } else if (compChosenHand == "👊🏻") && (chosinHand == "👊🏻") {
-                        
-                    } else if (compChosenHand == "✌🏻") && (chosinHand == "👊🏻") {
-                        userScore += 1
-                    }    else  if (compChosenHand == "✋🏻") && (chosinHand == "✌🏻") {
-                        userScore += 1
-                    } else if (compChosenHand == "👊🏻") && (chosinHand == "✌🏻") {
-                        compScore += 1
-                    } else if (compChosenHand == "✌🏻") && (chosinHand == "✌🏻") {
-                       
-                    }    else  if (compChosenHand == "✋🏻") && (chosinHand == "✋🏻") {
-                        
-                    } else if (compChosenHand == "👊🏻") && (chosinHand == "✋🏻") {
-                        userScore += 1
-                    } else if (compChosenHand == "✌🏻") && (chosinHand == "✋🏻") {
-                        compScore += 1
-                    }
-                    
-                    
+                         if (compChosenHand == "✋🏻") && (chosinHand == "👊🏻") { compScore += 1 }
+                    else if (compChosenHand == "✌🏻") && (chosinHand == "👊🏻") { userScore += 1 }
+                    else if (compChosenHand == "👊🏻") && (chosinHand == "✋🏻") { userScore += 1 }
+                    else if (compChosenHand == "✋🏻") && (chosinHand == "✌🏻") { userScore += 1 }
+                    else if (compChosenHand == "✌🏻") && (chosinHand == "✋🏻") { compScore += 1 }
+                    else if (compChosenHand == "👊🏻") && (chosinHand == "✌🏻") { compScore += 1 }
+    
                     UserHand.text = "\(chosinHand)"
                     CompHand.text = "\(compChosenHand)"
-                    
-                     if (compScore == 15) || (userScore == 15) {
-                    performSegue(withIdentifier: "page3", sender: nil)
-                    
                 }
-            }
-            
-            
+                     if (compScore == 3) || (userScore == 3) {
+                    performSegue(withIdentifier: "page3", sender: nil)
+                         
+                }
+                    
+                    if (compScore == 3) && (userScore < 3) {
+                        UserDefaults.standard.set("LOSSS", forKey: "data")
+                        print("if ")
+                        print("men - \(userScore)")
+                        print("compitur - \(compScore)")
+                        
+                       if let chap = UserDefaults.standard.object(forKey: "data") as? String {
+                            print(chap)
+                        }
+                       
+                    } else if (userScore == 3) && (compScore < 3) {
+                        UserDefaults.standard.set("WINNN", forKey: "data")
+                        print("else if")
+                        print("men - \(userScore)")
+                        print("compitur - \(compScore)")
+                        
+                        if let xan = UserDefaults.standard.object(forKey: "data") as? String {
+                             print(xan)
+                        }
+                        
+                    }
         }
-        
-        
-        
+ 
     }
-    
-    
-    
-    
+ 
 }
--- Rock-Paper-Scissors/ViewController3.swift
+++ Rock-Paper-Scissors/ViewController3.swift
@@ -11,13 +11,36 @@ class ViewController3: UIViewController {
 
     @IBOutlet weak var FinalLabel: UILabel!
   
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        sleep(2)
+        let data = UserDefaults.standard.object(forKey: "data")
+      
+        
+        if let Data = data as? String {
+            FinalLabel.text = "\(Data)"
+            print(Data)
+        } else {
+          print("data gelmedi")
+        }
+            
+       
+        
+   
     }
     
-
+    @IBAction func playAgainButton(_ sender: UIButton) {
+        
+       // UserDefaults.standard.removeObject(forKey: "data")
+        
+        performSegue(withIdentifier: "page1", sender: nil)
+    }
+    
 
 }
